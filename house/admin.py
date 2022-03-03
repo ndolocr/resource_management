74c8764b181187061f73dfb2cbf880c67b9563b2
ndolocr <crndolo@gmail.com>
2022-03-04 00:30:58 +0300

    Registering models for house App

--- house/admin.py
+++ house/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
+from house.models import Plot
+from house.models import Room
+from house.models import House
+from house.models import Tenant
+from house.models import RoomType
+from house.models import HouseType
+
 # Register your models here.
+
+admin.site.register(Plot)
+admin.site.register(Room)
+admin.site.register(House)
+admin.site.register(Tenant)
+admin.site.register(RoomType)
+admin.site.register(HouseType)
