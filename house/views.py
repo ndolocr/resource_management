264a89741668875ed515d04521c6254250d3ec78
ndolocr <crndolo@gmail.com>
2022-03-04 22:28:34 +0300

    layout for required view functions

--- house/views.py
+++ house/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def view_all_plots(request):
+    pass
+
+def view_all_houses(request):
+    pass
+
+def view_all_rooms(request):
+    pass
+
+def view_all_room_types(request):
+    pass
+
+def view_all_house_types(request):
+    pass
+
+def view_all_tenants(request):
+    pass
