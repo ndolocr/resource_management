87167d825ba71e3dd0de3937777275900a88a3a2
ndolocr <crndolo@gmail.com>
2022-03-25 22:45:17 +0300

    Creating Login page

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,6 @@ from core import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.dashboard, name='index-page'),
+    path('', views.login_view, name='login'),
+    path('dashboard', views.dashboard, name='dashboard'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,31 @@
+from multiprocessing import context
+from django.contrib import messages
+
 from django.shortcuts import render
+from django.shortcuts import redirect
+
+from django.contrib.auth import login
+from django.contrib.auth import logout
+from django.contrib.auth import authenticate
 
 # Create your views here.
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username, password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('dasboard')
+        else:
+            messages.success(request, ("Invalid login credentials!"))
+            return redirect('login')
+    else:        
+        context={}
+        return render(request, 'core/login.html', context)
+
 def dashboard(request):
     return render(request, 'core/dashboard.html')
+
