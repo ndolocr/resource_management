213a312341ef9dc8dde10f066585d899cf7134c3
ndolocr <crndolo@gmail.com>
2022-03-30 12:17:50 +0300

    Restructuring views and url paths for plot information

--- house/urls.py
+++ house/urls.py
@@ -5,5 +5,5 @@ from house import views
 
 urlpatterns =[
     path('all-plots/', views.view_all_plots, name='all-plots'),
-    path('view-plot/<int:id>', views.view_single_plot, name='view-plot'),
+    path('view-plot/<int:id>', views.view_plot, name='view-plot'),
 ]
--- house/views.py
+++ house/views.py
@@ -1,13 +1,27 @@
+from multiprocessing import context
 from django.shortcuts import render
 
 from house.models import Plot
 # Create your views here.
 
 def view_all_plots(request):
-    plots = Plot.objects.all()
-    
-    context = { "plots": plots }
-    return render(request, "house/all-plots.html", context)
+    try:
+        plots = Plot.objects.all()
+
+        context = { "plots": plots }
+        return render(request, "house/all-plots.html", context)
+    except Plot.DoesNotExist:
+        context={ "plot": "Error Accessing Plot Infromtion"}
+        return render(request, "house/all-plots.html", context)
+
+def view_plot(request, id):
+    try:
+        plot = Plot.objects.get(pk=id)
+        context = {"plot": plot}
+        return render(request, "house/view-plot.html", context)
+    except Plot.DoesNotExist:        
+        context = {"plot": "Error Accessing Plot Information"}
+        return render(request, "house/view-plot.html", context)
 
 def view_all_houses(request):
     pass
@@ -25,9 +39,6 @@ def view_all_tenants(request):
     pass
 
 def view_single_house(request):
-    pass
-
-def view_single_plot(request):
     pass
 
 def view_single_room(request):
